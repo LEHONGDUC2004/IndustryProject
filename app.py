b4311f5a248704df1355f1d8e0d6ecc5a477801b
LEHONGDUC2004 <lehongduc20122004@gmail.com>
2025-07-14 20:14:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ UPLOAD_DIR = os.environ.get("UPLOAD_DIR", "/data/uploaded")
 EXTRACT_DIR = os.environ.get("EXTRACT_DIR", "/data/extracted")
 REPLACED_DIR = os.environ.get("REPLACED_DIR", "/data/replaced")
 
-# Jenkins
+# Jenkins API
 JENKINS_URL = 'http://192.168.202.1:8081/job/build-web-static/buildWithParameters'
 JENKINS_USER = 'lehongduc3491'
 JENKINS_API_TOKEN = '110eaba63ed58b2bf4c17121b75c764984'
@@ -32,6 +32,7 @@ for path in [UPLOAD_DIR, EXTRACT_DIR, REPLACED_DIR]:
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Trả về trang upload.html
 @app.route('/')
 def index():
     return render_template('upload.html')
@@ -39,11 +40,11 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_zip():
     if 'file' not in request.files:
-        return '❌ No file part', 400
+        return 'No File', 400
 
     file = request.files['file']
     if file.filename == '' or not allowed_file(file.filename):
-        return '❌ Invalid file', 400
+        return 'Invalid file', 400
 
     filename = secure_filename(file.filename)
     zip_path = os.path.join(UPLOAD_DIR, filename)
@@ -53,38 +54,40 @@ def upload_zip():
     project_extract_path = os.path.join(EXTRACT_DIR, project_name)
     os.makedirs(project_extract_path, exist_ok=True)
 
-    # ✅ Giải nén
+    # Giải nén file zip ra
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(project_extract_path)
 
-    # ✅ Thêm Dockerfile
+    # Thêm Dockerfile cho file zip vừa giải nénnén
     dockerfile_path = os.path.join(project_extract_path, 'Dockerfile')
+    # Mẫu Dockerfile auto thêm vào file
     with open(dockerfile_path, 'w') as f:
         f.write("""\
-FROM nginx:stable-alpine
-COPY . /usr/share/nginx/html
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
-""")
+           FROM nginx:stable-alpine
+           COPY . /usr/share/nginx/html
+           EXPOSE 80
+           CMD ["nginx", "-g", "daemon off;"]
+    """)
 
-    # ✅ Nén lại
+    # Nén lại file zip vừa thêm Dockerfile
     replaced_zip_path = os.path.join(REPLACED_DIR, filename)
     shutil.make_archive(replaced_zip_path.rsplit('.', 1)[0], 'zip', project_extract_path)
 
-    logger.info(f"✅ Đã xử lý và lưu ZIP: {filename}")
+    logger.info(f" Đã xử lý và lưu ZIP: {filename}")
     trigger_jenkins_build(filename)
     return redirect('/')
 
 def trigger_jenkins_build(zip_filename):
     payload = {
-        'ZIP_NAME': zip_filename  # truyền tên file chứ không truyền path
+    # Truyền tên file chứ không truyền path
+        'ZIP_NAME': zip_filename  
     }
     response = requests.post(
         JENKINS_URL,
         auth=(JENKINS_USER, JENKINS_API_TOKEN),
         params=payload
     )
-    logger.info(f"📦 Trigger Jenkins với {zip_filename} - Status: {response.status_code}")
+    logger.info(f" Kích hoạt Jenkins với {zip_filename} - Status: {response.status_code}")
     return response.status_code
 
 if __name__ == '__main__':
