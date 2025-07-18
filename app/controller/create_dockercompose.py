5d01806e28f31bfe64ac6d4e6b867c75823a1922
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-19 02:31:32 +0700

    database

--- app/controller/create_dockercompose.py
+++ app/controller/create_dockercompose.py
@@ -6,40 +6,40 @@ def create_compose(docker_path):
 
     with open(compose_path, 'w') as f:
         f.write("""\
-    version: '3.8'
+version: '3.8'
 
-    services:
-      app:
-        build:
-          context: .
-          dockerfile: Dockerfile
-        container_name: host_app
-        ports:
-          - "5000:5000"
-        depends_on:
-          - db
-        environment:
-          - DB_HOST=db
-          - DB_USER=root
-          - DB_PASSWORD=123456
-          - DB_NAME=sample_db
+services:
+  app:
+    build:
+      context: .
+      dockerfile: Dockerfile
+    container_name: host_app
+    ports:
+      - "5000:5000"
+    depends_on:
+      - db
+    environment:
+      - DB_HOST=db
+      - DB_USER=root
+      - DB_PASSWORD=abc123@!
+      - DB_NAME=sample_db
 
-      db:
-        image: mysql:5.7
-        container_name: flask_mysql_host
-        restart: always
-        environment:
-          MYSQL_ROOT_PASSWORD: 123456
-          MYSQL_DATABASE: sample_db
-        ports:
-          - "3306:3306"
-    """)
+  db:
+    image: mysql:5.7
+    container_name: flask_mysql_host
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: abc123@!
+      MYSQL_DATABASE: sample_db
+    ports:
+      - "3306:3306"
+""")
+
         if has_init_sql:
             f.write("    volumes:\n")
             f.write("      - ./init.sql:/docker-entrypoint-initdb.d/init.sql\n")
 
-        f.write("""
-    volumes:
-      db_data:
-    """)
-
+        f.write("""\
+volumes:
+  db_data:
+""")
