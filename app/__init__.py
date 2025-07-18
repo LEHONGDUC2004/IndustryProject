bd1a4729cfab1392fea53ba69e71b1f0895fcd6d
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-18 20:55:35 +0700

    version 3- test - not login

--- app/__init__.py
+++ app/__init__.py
@@ -27,5 +27,4 @@ def create_app():
     from app.routes import register_routes
     register_routes(app)
 
-
     return app
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app, db
 
 app = create_app()
-with app.app_context():
-    db.create_all()
-    print("Database và các bảng đã được khởi tạo!")
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000)
