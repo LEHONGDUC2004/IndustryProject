71ff07ff77b702b001804317c0cea2d775e4f81f
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-18 22:31:00 +0700

    5

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,7 @@ from app.models import Account
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret123'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@db/upload_app'
-    # database mysql localhost
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456789a@localhost/db?charset=utf8mb4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@db/upload_app'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
