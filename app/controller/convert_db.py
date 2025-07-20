b05e42f8eb53c8b8a573d20c734c35267e4e420f
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-20 20:46:23 +0700

    new version

--- app/controller/convert_db.py
+++ app/controller/convert_db.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def import_sql_to_mysql(sql_path, db_info):
+    """
+    Import file .sql vào container MySQL bằng docker exec
+    """
     command = [
         'mysql',
         f"-h{db_info['DB_HOST']}",
@@ -9,9 +12,16 @@ def import_sql_to_mysql(sql_path, db_info):
         db_info['DB_NAME']
     ]
 
+    # Tên container MySQL đúng
+    docker_command = ['docker', 'exec', '-i', 'flask_mysql_host'] + command
 
-    docker_command = ['docker', 'exec', '-i', 'flask_app_container_name'] + command
-
-    with open(sql_path, 'rb') as f:
-        process = subprocess.Popen(docker_command, stdin=f)
-        process.wait()
+    try:
+        with open(sql_path, 'rb') as f:
+            process = subprocess.Popen(docker_command, stdin=f)
+            process.wait()
+            if process.returncode == 0:
+                print("Import .sql thành công")
+            else:
+                print("Import thất bại với mã lỗi", process.returncode)
+    except FileNotFoundError as e:
+        print("Lệnh 'docker' không tồn tại. Có thể đang chạy trong container không có docker CLI.")
