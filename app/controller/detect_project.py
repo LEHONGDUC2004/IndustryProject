72a91a4e76add6e33c5a95749d3202f75abc2073
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-21 14:34:34 +0700

    new detect-project

--- app/controller/detect_project.py
+++ app/controller/detect_project.py
@@ -1,21 +1,17 @@
 import os
 
 def detect_project_type(project_path):
-    has_json = False
     has_py = False
     has_html = False
 
-    for filename in os.listdir(project_path):
-        if filename.endswith('.json'):
-            has_json = True
-        elif filename.endswith('.py'):
-            has_py = True
-        elif filename.endswith('.html'):
-            has_html = True
+    for root, dirs, files in os.walk(project_path):
+        for filename in files:
+            if filename.endswith('.py'):
+                has_py = True
+            elif filename.endswith('.html'):
+                has_html = True
 
-    if has_json:
-        return 'nodejs'
-    elif has_py:
+    if has_py:
         return 'flask'
     elif has_html:
         return 'static'
