eade46debee8a335b0e13bc1f61fed033ce8825c
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-07-21 15:20:04 +0700

    new

--- app/controller/find_init_file.py
+++ app/controller/find_init_file.py
@@ -1,17 +1,14 @@
 import os
 
-def find_flask_app_file(base_path):
-    for root, dirs, files in os.walk(base_path):
+def find_executable_python_file(project_path):
+    for root, _, files in os.walk(project_path):
         for file in files:
             if file.endswith('.py'):
-                file_path = os.path.join(root, file)
-                try:
-                    with open(file_path, 'r', encoding='utf-8') as f:
-                        content = f.read()
-                        # Điều kiện tìm Flask app hoặc SQLAlchemy URI
-                        if "Flask(__name__)" in content or "SQLALCHEMY_DATABASE_URI" in content:
-                            return file_path
-                except Exception as e:
-                    print(f"[!] Bỏ qua file {file_path} do lỗi: {e}")
-                    continue
+                full_path = os.path.join(root, file)
+                with open(full_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    if '__main__' in content and 'app.run' in content:
+                        # Trả về đường dẫn tính từ thư mục gốc Docker (/app)
+                        return os.path.relpath(full_path, project_path)
     return None
+
