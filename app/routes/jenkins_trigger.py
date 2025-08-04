4cd85193f7ec2272daf590e1e9e648c526dc0878
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-05 03:59:37 +0700

    test user

--- app/routes/jenkins_trigger.py
+++ app/routes/jenkins_trigger.py
@@ -5,7 +5,7 @@ upload_bp = Blueprint('upload', __name__)
 logger = logging.getLogger(__name__)
 
 
-JENKINS_URL = 'http://192.168.202.1:8081/job/build-web-static/buildWithParameters'
+JENKINS_URL = 'http://3.212.74.20:8080/job/build-web-static/buildWithParameters'
 JENKINS_USER = 'lehongduc3491'
 # JENKINS_API_TOKEN = '110eaba63ed58b2bf4c17121b75c764984'
 JENKINS_API_TOKEN = '11e592530e49b4dde7bdf44ee65b6e9685'
