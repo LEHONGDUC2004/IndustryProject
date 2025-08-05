845bc958b2357074d4432ace4438a02ed3053776
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-05 19:42:53 +0700

    new

--- app/static/js/style.js
+++ app/static/js/style.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   const deployForm = document.getElementById('deploy-form');
 
@@ -11,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const fileSql = document.getElementById('file_sql');
   const passwd = document.getElementById('passwd');
   const confirmPasswd = document.getElementById('confirm_passwd');
+  const loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
 
   // Ẩn/hiện trường theo lựa chọn CSDL
   function toggleFields() {
@@ -35,8 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
     if (optionExisting.checked) {
       const pass1 = passwd.value.trim();
       const pass2 = confirmPasswd.value.trim();
-      // trả về giá trị mặt định value
-      document.getElementById('host_db').value = "";
+      document.getElementById('host_db').value = ""; // reset
+
       if (pass1.length < 6) {
         e.preventDefault();
         alert("Mật khẩu phải có ít nhất 6 ký tự.");
@@ -50,16 +50,13 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
 
-    // Nếu muốn: alert khi thành công (có thể xoá nếu bạn submit thật)
-    alert("Tải lên và triển khai thành công!");
+    // Hiện modal loading
+    loadingModal.show();
   });
 });
-
-window.onload = function() {
-    setInterval(function() {
-      var frame = document.getElementById("jenkins-frame");
-      frame.src = frame.src;
-    }, 10000);
+window.onload = function () {
+  setInterval(function () {
+    const frame = document.getElementById("jenkins-frame");
+    frame.src = frame.src;
+  }, 10000);
 };
-
-
