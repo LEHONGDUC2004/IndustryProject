162ddf374bd3e808436b553923e833d10da916d8
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-05 16:19:20 +0700

    new

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,8 @@
 from app.routes.main_route import main_bp
 from app.routes.upload_all import uploadAll_bp
-from app.routes.jenkins_trigger import jenkins_bp
+
 
 def register_routes(app):
     app.register_blueprint(main_bp, url_prefix='/')
     app.register_blueprint(uploadAll_bp, url_prefix='/upload_all')
-    app.register_blueprint(jenkins_bp, url_prefix='/jenkins_bp')
+
