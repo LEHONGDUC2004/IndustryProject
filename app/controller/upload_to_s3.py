279cc3e6fca8a52d00e5ddfde65fa96c0338237f
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-10 00:08:37 +0700

    new

--- app/controller/upload_to_s3.py
+++ app/controller/upload_to_s3.py
@@ -6,10 +6,7 @@ s3 = boto3.client('s3')
 S3_BUCKET = 'duckou-myproject'
 
 def upload_to_s3(file_path, filename, user_id, project_id):
-    # Tạo prefix theo user và ngày giờ để tránh trùng
-    timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    s3_key = f"uploads/user-{user_id}/project-{project_id}/{filename}"
-
+    s3_key = f"source-code/user-{user_id}/project-{project_id}/{filename}"
     s3.upload_file(file_path, S3_BUCKET, s3_key)
     return s3_key
 
