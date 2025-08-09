a55765eac97c8608b32b5650a65e72070546a1e5
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-09 23:49:13 +0700

    new

--- app/controller/upload_to_s3.py
+++ app/controller/upload_to_s3.py
@@ -5,10 +5,13 @@ from datetime import datetime
 s3 = boto3.client('s3')
 S3_BUCKET = 'duckou-myproject'
 
-def upload_to_s3(file_path, filename, user_id):
+def upload_to_s3(file_path, filename, user_id, project_id):
     # Tạo prefix theo user và ngày giờ để tránh trùng
     timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    s3_key = f"uploads/user-{user_id}/{timestamp}_{filename}"
+    s3_key = f"uploads/user-{user_id}/project-{project_id}/{filename}"
 
     s3.upload_file(file_path, S3_BUCKET, s3_key)
     return s3_key
+
+def get_old_version(bucket, key, version_id, dest):
+    s3.download_file(bucket, key, dest, ExtraArgs={"VersionId": version_id})
