d1483a09fa6e3966867a9db9facc0d6888fd6d6d
LEHONGDUC2004 <lehongduc201204@gmail.com>
2025-08-10 23:19:06 +0700

    nbew

--- app/routes/jenkins_trigger.py
+++ app/routes/jenkins_trigger.py
@@ -1,22 +1,20 @@
 from flask import Blueprint, render_template
 import requests, logging
-from urllib.parse import urljoin
 from app.controller.config import JENKINS_BASE_URL, JENKINS_USER, JENKINS_API_TOKEN
 upload_bp = Blueprint('upload', __name__)
 logger = logging.getLogger(__name__)
 
 
+
 session = requests.session()
 session.auth = (JENKINS_USER, JENKINS_API_TOKEN)
 session.headers.update({"User-Agent": "upload-service/1.0", "Accept": "application/json"})
+JENKINS_JOB_URL = f"{JENKINS_BASE_URL}/job/download-code-from-s3/buildWithParameters"
+JENKINS_VIEW_URL = f"{JENKINS_BASE_URL}/view/MyView"
+session = requests.session()
+session.auth = (JENKINS_USER, JENKINS_API_TOKEN)
+session.headers.update({"User-Agent": "upload-service/1.0"})
 
-def get_crumb():
-    # /crumbIssuer/api/json trả về {"crumbRequestField":"Jenkins-Crumb","crumb":"..."}
-    url = urljoin(JENKINS_BASE_URL + "/", "crumbIssuer/api/json")
-    r = session.get(url, timeout=10)
-    r.raise_for_status()
-    data = r.json()
-    return {data["crumbRequestField"]: data["crumb"]}
 
 # Trigger build with ZIP_NAME + S3_KEY
 def trigger_jenkins_build(zip_filename, s3_key):
@@ -30,5 +28,10 @@ def trigger_jenkins_build(zip_filename, s3_key):
         zip_filename, s3_key, r.status_code
     )
     return r.status_code
+
+
+
+
+
 
 
